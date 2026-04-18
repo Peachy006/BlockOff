e58661d6af819ab264625bd7ad742c684c5f62dc
= <seanpettas@gmail.com>
2026-04-18 11:20:16 +0200

    first commit

---
+++ src/main/java/com/example/blockoff/BlockOffApplication.java
@@ -0,0 +1,13 @@
+package com.example.blockoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockOffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockOffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blockoff/BlockOffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blockoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockOffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
