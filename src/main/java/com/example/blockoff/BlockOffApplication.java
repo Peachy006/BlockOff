8d1b7e2e81730ca4866cc27e38ab4ddfdd38aaf7
= <seanpettas@gmail.com>
2026-04-18 11:23:08 +0200

    test

--- src/main/java/com/example/blockoff/BlockOffApplication.java
+++ src/main/java/com/example/blockoff/BlockOffApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlockOffApplication {
-
+    //test
     public static void main(String[] args) {
         SpringApplication.run(BlockOffApplication.class, args);
     }
